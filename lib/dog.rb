5fdbd7f5eefaf31ea4b5c9d47c446c8e2a542082
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-09 15:08:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_accessor :name, :breed
   attr_reader :id
-  
+
   def initialize(id = nil, name, breed)
     @id = id
     @name = name
@@ -10,12 +10,32 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE dogs (
+      CREATE TABLE IF NOT EXISTS dogs (
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
       )
     SQL
     DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+
+  def self.new_from_db(row)
+    dog = self.new(row[0], row[1], row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * 
+      FROM dogs 
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
