85b4d9bc9674a0b703f11d4e8af7e375126a6d0a
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-09 15:12:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,9 +40,17 @@ class Dog
   end
 
   def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
   def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 
   
